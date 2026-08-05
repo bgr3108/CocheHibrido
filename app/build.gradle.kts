dacf205d08dc61ca56487e033457812473b75d3b
bgr3108 <borja.g.roque@gmail.com>
2026-08-05 19:53:37 +0100

    build: clean Gradle version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,9 @@ dependencies {
     // DATASTORE
     implementation(libs.androidx.datastore.preferences)
 
-    testImplementation("junit:junit:4.13.2")
+    testImplementation(libs.junit)
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
     // tests...
 }
