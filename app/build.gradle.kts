1d41b76408adec26570cd4c6fe55724187bf54fe
bgr3108 <borja.g.roque@gmail.com>
2026-08-08 04:36:40 +0100

    build: enable release minification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,17 @@ android {
     buildFeatures {
         compose = true
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 }
 
 kotlin {
