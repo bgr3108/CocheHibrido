8f6806bf78a15970ec62776b6dac87a81b1eae10
bgr3108 <borja.g.roque@gmail.com>
2026-08-25 17:56:38 +0100

    chore: prepare release 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bgr3108.kilonom"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
